8e20a63b68ab21e9f9813d07351cbd5b9d870f3a
ShreeShukla191 <shreeshukla56@gmail.com>
2024-09-20 12:37:34 +0530

    Add files via upload

---
+++ FLOAT2.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a=9,b=5;
+ printf("Addition=%d\n",a+b);
+ printf("Subtraction=%d\n",a-b);
+ printf("Multiplication=%d\n",a*b);
+ printf("Division=%d\n",a/b);
+ if(a>b)
+ {
+  printf("a is greater than b\n");
+  }
+ else
+ {
+ printf("b is greater than a\n");
+ }
+getch();
+clrscr();
+}
