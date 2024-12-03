57a82d624228f8bac3b17c20378046742cc8bd2a
ShreeShukla191 <shreeshukla56@gmail.com>
2024-12-03 16:04:43 +0530

    Add files via upload

---
+++ structure_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	struct student
+	{
+		int roll_no;
+		float total_marks;
+		char name[50];
+	};
+	struct student stud[100];
+	int n,i;
+	printf("Enter total number of students:\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("Enter details of student %d:\n",(i+1));
+		printf("Name:\n");
+		scanf("%s",&stud[i].name);
+		printf("Roll no.:\n");
+		scanf("%d",&stud[i].roll_no);
+		printf("Total marks:\n");
+		scanf("%f",&stud[i].total_marks);
+	}
+    printf("STUDENTS DETAILS:\n\n");
+	for(i=0;i<n;i++)
+	{
+	printf("Roll no.:%d\nName:%s\ntotal_Marks:%.2f\n\n",stud[i].roll_no,stud[i].name,stud[i].total_marks);
+    }
+	return 0;
+}
